17b2453f9935c0778dd7de551e3d1d91aa798cf2
Will <will_constable@msn.com>
2021-07-23 16:45:52 +0100

    commit config

---
+++ src/entry-server.js
@@ -0,0 +1,25 @@
+import { createApp } from './main.js'
+
+export default context => {
+  return new Promise((resolve, reject) => {
+    const { app, router, store } = createApp()
+    const meta = app.$meta()
+
+    router.push(context.url)
+
+    context.meta = meta
+
+    router.onReady(() => {
+      context.rendered = () => {
+        context.state = store.state
+      }
+
+      const matchedComponents = router.getMatchedComponents()
+      if (!matchedComponents.length) {
+        return reject(new Error(404))
+      }
+
+      return resolve(app)
+    }, reject)
+  })
+}
---
+++ src/store/const/main.js
@@ -0,0 +1,6 @@
+export const MAIN__ITEM_DELETE = 'MAIN__ITEM_DELETE'
+export const MAIN__ITEM_ADD = 'MAIN__ITEM_ADD'
+export const MAIN__ITEM_ADD_ASYNC = 'MAIN__ITEM_ADD_ASYNC'
+
+export const MAIN__GET_DATA = 'MAIN__GET_DATA'
+export const MAIN__SET_DATA = 'MAIN__SET_DATA'
